1b56f2dfcafef16abcdc16d2a5ceee28e90291f0
HimaSagar12 <jsagarhima@gmail.com>
2025-07-31 21:12:27 +0530

    cors error

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import engine, Base
 from .routers import users, pocs, applications, comments, reviews, token
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# CORS Middleware
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Routers
 app.include_router(users.router, prefix="/users", tags=["users"])
