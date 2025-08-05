5990d2a7245e007859ed48f7c4764c30a0f43569
HimaSagar12 <jsagarhima@gmail.com>
2025-08-05 18:10:35 +0530

    replaced brython with streamlib

--- backend/routers/pocs.py
+++ backend/routers/pocs.py
@@ -4,15 +4,13 @@ from typing import List
 from .. import crud, schemas, models
 from ..dependencies import get_db, get_current_user
 
-router = APIRouter(
-    tags=["pocs"],
-)
+router = APIRouter()
 
 @router.post("/", response_model=schemas.POC)
 def create_poc(poc: schemas.POCCreate, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
     return crud.create_poc(db=db, poc=poc, owner_id=current_user.id)
 
-@router.get("/", response_model=List[schemas.POC])
+@router.get("", response_model=List[schemas.POC])
 def read_pocs(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     pocs = crud.get_pocs(db, skip=skip, limit=limit)
     return pocs
