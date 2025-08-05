b971c5d90921ebd126a16fb1b341c714930a4501
HimaSagar12 <jsagarhima@gmail.com>
2025-08-05 18:28:22 +0530

    adding all user details retrieving endpoint

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
 from .. import crud, schemas, models
 from ..dependencies import get_db, get_current_user
 
@@ -11,6 +12,11 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
+
+@router.get("", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
 @router.get("/me", response_model=schemas.User)
 def read_users_me(current_user: models.User = Depends(get_current_user)):
