41d21f9d0c621ad3743c581d2e885550dd4f9ebd
HimaSagar12 <jsagarhima@gmail.com>
2025-08-06 17:47:22 +0530

    api endpoint

--- backend/routers/comments.py
+++ backend/routers/comments.py
@@ -8,7 +8,7 @@ router = APIRouter(
     prefix="/comments",
 )
 
-@router.post("/", response_model=schemas.Comment)
+@router.post("", response_model=schemas.Comment)
 def create_comment(comment: schemas.CommentCreate, db: Session = Depends(get_db)):
     return crud.create_comment(db=db, comment=comment, author_id=comment.author_id)
 
