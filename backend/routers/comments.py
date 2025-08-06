4ee2d1d63b2adafaf04e87fc9c7971b4850a6814
HimaSagar12 <jsagarhima@gmail.com>
2025-08-06 18:14:18 +0530

    comments error

--- backend/routers/comments.py
+++ backend/routers/comments.py
@@ -4,16 +4,13 @@ from typing import List
 from .. import crud, schemas, models
 from ..dependencies import get_db
 
-router = APIRouter(
-    prefix="/comments",
-)
+router = APIRouter()
 
-@router.post("", response_model=schemas.Comment)
-@router.post("/", response_model=schemas.Comment)
+@router.post("/comments", response_model=schemas.Comment)
 def create_comment(comment: schemas.CommentCreate, db: Session = Depends(get_db)):
     return crud.create_comment(db=db, comment=comment, author_id=comment.author_id)
 
-@router.get("/poc/{poc_id}", response_model=List[schemas.Comment])
+@router.get("/comments/poc/{poc_id}", response_model=List[schemas.Comment])
 def read_comments_for_poc(poc_id: int, db: Session = Depends(get_db)):
     comments = crud.get_comments_for_poc(db, poc_id=poc_id)
     return comments
